3ee0770b737e19c9bffa27412ae26cfbf5ec44f6
jesusveca <jerikerl10@gmail.com>
2016-06-08 20:44:20 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 using namespace std;
-#define MAX 20
 
 class matrix{
     private:
-        int n;
+        int nodos;
         int **adjunto;
         bool *visita;
     public:
-        matrix(int n){
-            this->n = n;
-            visita = new bool [n];
-            adjunto = new int* [n];
-            for (int i = 0; i < n; i++){
-                adjunto[i] = new int [n];
-                for(int j = 0; j < n; j++){
+        matrix(int nodos){
+            this->nodos = nodos;
+            visita = new bool [nodos];
+            adjunto = new int* [nodos];
+            for (int i = 0; i < nodos; i++){
+                adjunto[i] = new int [nodos];
+                for(int j = 0; j < nodos; j++){
                     adjunto[i][j] = 0;
                 }
             }
         }
 
         void anadir_arista(int inicio, int finall){
-            if( inicio > n || finall > n || inicio < 0 || finall < 0)
+            if( inicio > nodos || finall > nodos || inicio < 0 || finall < 0)
                 cout<<"no valido!\n";
             else{
                 adjunto[inicio - 1][finall - 1] = 1;
@@ -30,8 +29,8 @@ class matrix{
 
         void imprimir(){
             int i,j;
-            for(i = 0;i < n;i++){
-                for(j = 0; j < n; j++)
+            for(i = 0;i < nodos;i++){
+                for(j = 0; j < nodos; j++)
                     cout<<adjunto[i][j]<<"  ";
                 cout<<endl;
             }
